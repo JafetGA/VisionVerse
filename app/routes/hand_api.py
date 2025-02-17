5290c31f862e8f8ce4d42106eff31c759a03aeb5
Jafet <eli.jafet.ga@gmail.com>
2025-02-17 00:03:13 -0600

    Implement hand detection in API; decode image from base64 and return hand coordinates

--- app/routes/hand_api.py
+++ app/routes/hand_api.py
@@ -1,8 +1,18 @@
 from flask import Blueprint, Response, Request, jsonify, request
+import cv2, base64
+import numpy as np
+from app.services.hand_detection import HandDetector
 
 api = Blueprint('api', __name__)
 
 @api.route('/detect_hand', methods=['POST'])
 def hand_stimation():
     data = request.get_json()
-    return jsonify(data)
+    frame_str = data['frame'].split(',')[1]
+    decoded_img = base64.b64decode(frame_str)
+    np_arr = np.frombuffer(decoded_img, np.uint8)
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    detector = HandDetector()
+    results = detector.find_hands(img)
+    return jsonify({"coordinates": results["coordinates"]})
